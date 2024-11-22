cc9685f54749685e9d3a44a3577d5d6e243301ed
Julian Tabona <brandontabona@gmail.com>
2024-11-22 11:34:08 +0200

    Update 22 Nov 2024 11:34

--- resources/js/Pages/SubscriptionPlans/List/Partials/ManageSubscriptionPlanModal.vue
+++ resources/js/Pages/SubscriptionPlans/List/Partials/ManageSubscriptionPlanModal.vue
@@ -787,8 +787,8 @@
                         delete data.auto_billing_reminder_ids;
                         return data;
                     }else {
-                        if(data.billing_purchase_category_code.trim() == '') delete data.billing_purchase_category_code;
-                        if(data.billing_product_id.trim() == '') delete data.billing_product_id;
+                        if(data.billing_purchase_category_code == null || data.billing_purchase_category_code.trim() == '') delete data.billing_purchase_category_code;
+                        if(data.billing_product_id == null || data.billing_product_id.trim() == '') delete data.billing_product_id;
                         return data;
                     }
                 }).post(route('create.subscription.plan', { project: route().params.project }), options);
